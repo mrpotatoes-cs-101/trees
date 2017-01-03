d2c84aa8d147c8f0482ffa9a84725c9b5c6284a3
Andric LibreSinn <contact@andricvillanueva.com>
2017-01-03 15:42:12 -0500

    Adding in a bunch of convenience methods

--- src/main/java/com/mrpotatoes/cs101/trees/binarytree/node/TraverseTree.java
+++ src/main/java/com/mrpotatoes/cs101/trees/binarytree/node/TraverseTree.java
@@ -5,55 +5,55 @@ package com.mrpotatoes.cs101.trees.binarytree.node;
  * @author alibresinn
  */
 public class TraverseTree {
-  /**
-   *
-   * Secretly this is the pre-order traverse. GASP!
-   * @param focusNode
-   */
-  public void traverse(Node focusNode) {
-    if (focusNode != null) {
-			System.out.println(focusNode);
-
-			this.traverse(focusNode.leftChild);
-			this.traverse(focusNode.rightChild);
-		}
-  }
-
-  /**
-   *
-   * @param focusNode
-   */
-  public void inOrderTraverseTree(Node focusNode) {
-    if (focusNode != null) {
-			// Traverse the left node
-			this.traverse(focusNode.leftChild);
-
-			// Visit the currently focused on node
-			System.out.println(focusNode);
-
-			// Traverse the right node
-			this.traverse(focusNode.rightChild);
-		}
-  }
-
-  /**
-   *
-   * @param focusNode
-   */
-  public void preorderTraverseTree(Node focusNode) {
-    this.traverse(focusNode);
-  }
-
-  /**
-   *
-   * @param focusNode
-   */
-  public void postOrderTraverseTree(Node focusNode) {
-    if (focusNode != null) {
-			this.traverse(focusNode.leftChild);
-			this.traverse(focusNode.rightChild);
-
-			System.out.println(focusNode);
-		}
-  }
+//  /**
+//   *
+//   * Secretly this is the pre-order traverse. GASP!
+//   * @param focusNode
+//   */
+//  public void traverse(Node focusNode) {
+//    if (focusNode != null) {
+//			System.out.println(focusNode);
+//
+//			this.traverse(focusNode.leftChild);
+//			this.traverse(focusNode.rightChild);
+//		}
+//  }
+//
+//  /**
+//   *
+//   * @param focusNode
+//   */
+//  public void inOrderTraverseTree(Node focusNode) {
+//    if (focusNode != null) {
+//			// Traverse the left node
+//			this.traverse(focusNode.leftChild);
+//
+//			// Visit the currently focused on node
+//			System.out.println(focusNode);
+//
+//			// Traverse the right node
+//			this.traverse(focusNode.rightChild);
+//		}
+//  }
+//
+//  /**
+//   *
+//   * @param focusNode
+//   */
+//  public void preorderTraverseTree(Node focusNode) {
+//    this.traverse(focusNode);
+//  }
+//
+//  /**
+//   *
+//   * @param focusNode
+//   */
+//  public void postOrderTraverseTree(Node focusNode) {
+//    if (focusNode != null) {
+//			this.traverse(focusNode.leftChild);
+//			this.traverse(focusNode.rightChild);
+//
+//			System.out.println(focusNode);
+//		}
+//  }
 }
